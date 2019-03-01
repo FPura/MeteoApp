d3d40939d9b8cbb11895ac7d1a6bfde93dd93623
Filippo <purafilippo@gmail.com>
2019-03-01 11:47:56 +0100

    add location

--- app/src/main/java/ch/supsi/dti/isin/meteoapp/activities/MainActivity.java
+++ app/src/main/java/ch/supsi/dti/isin/meteoapp/activities/MainActivity.java
@@ -1,13 +1,63 @@
 package ch.supsi.dti.isin.meteoapp.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import ch.supsi.dti.isin.meteoapp.fragments.ListFragment;
+import io.nlopez.smartlocation.OnLocationUpdatedListener;
+import io.nlopez.smartlocation.SmartLocation;
+import io.nlopez.smartlocation.location.config.LocationAccuracy;
+import io.nlopez.smartlocation.location.config.LocationParams;
 
 public class MainActivity extends SingleFragmentActivity {
 
+    private static final int REQ_CODE = 100;
+
     @Override
     protected Fragment createFragment() {
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i("", "Permission not granted");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQ_CODE);
+        } else {
+            Log.i("", "Permission is granted");
+            startLocationListener();
+        }
+
         return new ListFragment();
+    }
+
+    private void startLocationListener() {
+        LocationParams.Builder builder = new LocationParams.Builder()
+                .setAccuracy(LocationAccuracy.HIGH)
+                .setDistance(0)
+                .setInterval(5000); // 5 sec
+        SmartLocation.with(this).location().continuous().config(builder.build())
+                .start(new OnLocationUpdatedListener() {
+                    @Override
+                    public void onLocationUpdated(Location location) {
+                        Log.i("ciao", "Location" + location);
+
+                    }});
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQ_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // ho ottenuto i permessi
+                    Log.i("", "Permission was granted");
+                    startLocationListener();
+                }
+                return;
+            }
+        }
     }
 }
